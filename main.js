fd22145d5f2688ecfaa2ec0608852f85b89a8a8a
Devi30 <96772778+Devi30@users.noreply.github.com>
2023-04-02 16:07:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
 song1=""
 song2=""
 function preload(){
@@ -9,6 +13,22 @@ function setup(){
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
+    posenet=ml5.poseNet(video,modelloaded)
+    posenet.on('pose',gotposes)
+}
+function modelloaded(){
+    console.log("posenet is loaded")
+}
+function gotposes(results){
+ if(results.length>0){
+    console.log(results)
+    leftwristx=results[0].pose.leftWrist.x 
+    leftwristy=results[0].pose.leftWrist.y 
+    rightwristx=results[0].pose.rightWrist.x
+    rightwristy=results[0].pose.rightWrist.y
+    console.log("leftwristx="+leftwristx+" leftwristy="+leftwristy)
+    console.log("rightwristx="+rightwristx+" rightwristy="+rightwristy)
+ }
 }
 function draw(){
     image (video,0,0,600,500)
